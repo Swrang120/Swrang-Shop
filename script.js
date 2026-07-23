78bfe4c497dc92420f7bb14f616238c63f6a1583
Swrang120 <sboro3235@gmail.com>
2026-07-23 23:26:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,11 @@
-function shopNow() {
-  document.querySelector(".products").scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-// Search
-const search = document.querySelector(".search");
-
-if (search) {
-  search.addEventListener("keyup", function () {
-    let value = search.value.toLowerCase();
-    let products = document.querySelectorAll(".product");
-
-    products.forEach(function (item) {
-      let text = item.innerText.toLowerCase();
-      item.style.display = text.includes(value) ? "" : "none";
-    });
-  });
-}
-
-// Add To Cart
-document.querySelectorAll(".cart").forEach(function(button){
-
-  button.addEventListener("click", function(){
-
-    const product = this.parentElement.querySelector("h3").innerText;
-    const price = this.parentElement.querySelector(".price").innerText;
-
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-    cart.push({
-      product: product,
-      price: price
-    });
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-
-    alert(product + " Added To Cart");
-  });
-
-});
-
-// Buy Now
-document.querySelectorAll(".buy").forEach(function(button){
-
-  button.addEventListener("click", function(){
-    window.location.href = "checkout.html";
-  });
-
+document.querySelectorAll(".cart").forEach(function(button) {
+  button.onclick = function() {
+    localStorage.setItem("cart", JSON.stringify([
+      {
+        product: "Test Product",
+        price: "₹100"
+      }
+    ]));
+    alert("Saved");
+  };
 });
