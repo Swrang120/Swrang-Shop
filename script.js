bacfa00fb27915ba71a0cd647aefff607e7cb474
Swrang120 <sboro3235@gmail.com>
2026-07-23 20:16:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function shopNow(){
-alert("Welcome To Swrang Shop");
+function shopNow() {
+    document.querySelector(".products").scrollIntoView({
+        behavior: "smooth"
+    });
 }
+
+const search = document.querySelector(".search");
+
+search.addEventListener("keyup", function () {
+    let value = search.value.toLowerCase();
+    let products = document.querySelectorAll(".product");
+
+    products.forEach(function (item) {
+        let text = item.innerText.toLowerCase();
+
+        if (text.includes(value)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
