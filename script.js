94c62259458c06a7c4c4fa499b29cb9f0a987e53
Swrang120 <sboro3235@gmail.com>
2026-07-23 20:28:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function shopNow() {
-    document.querySelector(".products").scrollIntoView({
-        behavior: "smooth"
-    });
+  document.querySelector(".products").scrollIntoView({
+    behavior: "smooth"
+  });
 }
 
 const search = document.querySelector(".search");
 
-search.addEventListener("keyup", function () {
+if (search) {
+  search.addEventListener("keyup", function () {
     let value = search.value.toLowerCase();
     let products = document.querySelectorAll(".product");
 
     products.forEach(function (item) {
-        let text = item.innerText.toLowerCase();
+      let text = item.innerText.toLowerCase();
 
-        if (text.includes(value)) {
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
+      if (text.includes(value)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
     });
-});
+  });
+}
