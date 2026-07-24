34f32970b0718327dce11219d5cfedb2ce8b45c9
Swrang120 <sboro3235@gmail.com>
2026-07-24 09:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,10 @@ document.querySelectorAll(".buy").forEach(function (button) {
   });
 
 });
+function openMenu(){
+document.getElementById("sideMenu").style.width="260px";
+}
+
+function closeMenu(){
+document.getElementById("sideMenu").style.width="0";
+}
