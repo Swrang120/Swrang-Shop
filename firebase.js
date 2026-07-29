34a0b65f42ab7c6f88e6915a9a6298097884eb26
Swrang120 <sboro3235@gmail.com>
2026-07-29 13:04:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
-// Firebase App
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js"; "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-// Firebase Auth
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-// Firebase Database
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: AIzaSyDOJwiVff2D0KhfH3P14NT2q5c8dqLg9l8",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDOJwiVff2D0KhfH3P14NT2q5c8dqLg9l8",
+  authDomain: "ms-shopping-30823.firebaseapp.com",
+  databaseURL: "https://ms-shopping-30823-default-rtdb.firebaseio.com",
+  projectId: "ms-shopping-30823",
+  storageBucket: "ms-shopping-30823.firebasestorage.app",
+  messagingSenderId: "119548068283",
+  appId: "1:119548068283:web:4751f1df379b13fe76c49e",
+  measurementId: "G-185W4CLL57"
 };
 
 const app = initializeApp(firebaseConfig);
